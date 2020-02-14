b7f9f1ecce2db9ee91d1d79db9d35c84110d9539
YunHee Lee <yunheenet@gmail.com>
2020-02-14 10:21:49 +0900

    ADD Header

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { gql } from "apollo-boost";
 import styled, { ThemeProvider } from "styled-components";
-import { useQuery } from "react-apollo-hooks";
+import { HashRouter as Router } from "react-router-dom";
+import { useQuery } from "@apollo/react-hooks";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import GlobalStyles from "../Styles/GlobalStyles";
 import Theme from "../Styles/Theme";
-import Router from "./Router";
+import Routes from "./Routes";
+import Header from "./Header";
 
 const QUERY = gql`
   {
@@ -16,7 +18,7 @@ const QUERY = gql`
 
 const Wrapper = styled.div`
   margin: 0 auto;
-  max-width: 935px;
+  max-width: ${props => props.theme.maxWidth};
   width: 100%;
 `;
 
@@ -27,11 +29,18 @@ export default () => {
 
   return (
     <ThemeProvider theme={Theme}>
-      <Wrapper>
+      <>
         <GlobalStyles />
-        <Router isLoggedIn={isLoggedIn} />
+        <Router>
+          <>
+            <Header />
+            <Wrapper>
+              <Routes isLoggedIn={isLoggedIn} />
+            </Wrapper>
+          </>
+        </Router>
         <ToastContainer position={toast.POSITION.BOTTOM_LEFT} />
-      </Wrapper>
+      </>
     </ThemeProvider>
   );
 };
